c8e4d002e0f9793a1015bc2d9a27115a25bb68c6
mdiv <96177680+2div@users.noreply.github.com>
2024-08-03 06:48:31 +0000

    new update commit 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    images: {
+        unoptimized: true,
+      },
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
